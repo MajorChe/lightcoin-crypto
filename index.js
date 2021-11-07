29252467a4a54b4a506573032311286362780cc5
charit nanduri <charithnanduri03@gmail.com>
2021-11-07 18:12:41 +0000

    Update account using get balance

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 class Account {
   constructor(username) {
   this.username = username;
-  this.balance = 0;
+  this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    this.time = new Date();
+    for (let item of this.transactions) {
+      balance += item;
+      if(balance < 0) {
+        return "Insufficient balance to perform the withdrawal"
+      }
+    }
+    return balance +" "+ this.time;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 }
 
@@ -10,21 +24,22 @@ class Transfer {
     this.account = account;
     this.amount = amount;
   }
+  commit() {
+    //this.account.balance += this.value;
+    this.time = new Date();
+    this.account.transactions.push(this.value);
+  }
 }
 
 class Withdrawal extends Transfer{
-  commit() {
-    if(this.account.balance < this.amount){
-      return ('Insufficient Balance')
-    } else {
-      this.account.balance -= this.amount;
-    }
+  get value() {
+      return -this.amount;
   }
 }
 
 class Deposit extends Transfer{
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
@@ -34,9 +49,13 @@ class Deposit extends Transfer{
 const myAccount = new Account('MajorChe');
 t1 = new Deposit(myAccount,50.25);
 t1.commit();
-console.log('Updated account info of myAccount: ', myAccount);
+console.log('Updated account info of myAccount: ', myAccount.balance);
 
-t2 = new Withdrawal(myAccount,119.99);
+t2 = new Withdrawal(myAccount,11.99);
 t2.commit();
-//console.log('Updated account info of myAccount: ', myAccount);
+console.log('Updated account info of myAccount: ', myAccount.balance);
+
+t1 = new Deposit(myAccount,25.25);
+t1.commit();
+console.log('Updated account info of myAccount: ', myAccount.balance);
 
